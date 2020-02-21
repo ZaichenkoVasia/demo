6d8c4e8bc93267d68a107a48cd37640cabab6677
ZaichenkoVasia <zaichenko.vasyl@gmail.com>
2020-02-21 12:57:38 +0200

    demo third cherry

--- src/main/java/Master.java
+++ src/main/java/Master.java
@@ -1,10 +1,16 @@
 public class Master {
     public static void main(String[] args) {
         System.out.println();
-        System.out.println("2");
         System.out.println("3");
         System.out.println("4 - locale");
         System.out.println("4 - gh");
         System.out.println("4 - master");
+        System.out.println("32432");
+        System.out.println("5");
+        System.out.println("4 - locale");
+        System.out.println("4 - gh");
+        System.out.println("4 - master");
+        System.out.println("4 - demo2");
+        System.out.println("4 - demo3");
     }
 }
